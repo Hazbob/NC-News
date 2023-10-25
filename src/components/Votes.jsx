33c548678c42096d01586153a4f1d96e05eb5a18
harryRobinson <127695266+Hazbob@users.noreply.github.com>
2023-10-25 09:25:24 +0100

    completed vote functionality on the articles

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,4 +1,34 @@
-function Votes({votes}){
-    return <span><button>-</button>Votes: {votes} <button>+</button></span>
+import {useState} from "react";
+
+
+function Votes({votes, articleId}){
+    const [currentVote, setCurrentVote] = useState(votes)
+    const [disabled, setDisabled] = useState(false)
+
+    async function changeVotes(e){
+        try {
+        const incOrDec = e.target.innerText === "+" ? 1 : -1
+
+            setCurrentVote(currentVote + incOrDec)
+            const articleCopy = {article_id:articleId,
+                inc_votes: incOrDec
+            }
+            const res = await fetch(`https://be-northcoder-news.onrender.com/api/articles/${articleId}`, {
+                method: "PATCH",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(articleCopy)
+            })
+            const data = await res.json()
+            setDisabled(true)
+
+        }catch (err){
+            const errorMessage = "Try again Later..."
+            setCurrentVote(errorMessage)
+        }
+
+    }
+    return <span><button onClick={changeVotes} disabled={disabled}>-</button>Votes: {currentVote} <button onClick={changeVotes} disabled={disabled}>+</button></span>
 }
 export default Votes
