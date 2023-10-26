cf86acbc750cd93b6f19e7f89891dce5f03e7225
harryRobinson <127695266+Hazbob@users.noreply.github.com>
2023-10-26 13:42:50 +0100

    added more functionality to the post comments form validation

--- src/components/article_components/Comments.jsx
+++ src/components/article_components/Comments.jsx
@@ -1,43 +1,46 @@
-import {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import formatDate from "../../utils/formatDate.js";
 import Loader from "../Loader.jsx";
 import Votes from "../Votes.jsx";
 
+function Comments({ comments, setComments }) {
+  const [isLoading, setLoading] = useState(true);
 
-function Comments({comments, setComments}){
-    const [isLoading, setLoading] = useState(true)
+  const { id } = useParams();
 
-    const {id} = useParams()
+  async function getComments(articleId) {
+    const res = await fetch(
+      `https://be-northcoder-news.onrender.com/api/articles/${articleId}/comments`,
+    );
+    const { comments } = await res.json();
 
-    async function getComments(articleId){
-        const res = await fetch(`https://be-northcoder-news.onrender.com/api/articles/${articleId}/comments`)
-        const {comments} = await res.json()
+    const commentValues = comments.map((comment, index) => {
+      return (
+        <li className={"comment-card"} key={index + comment.body}>
+          <p>{comment.body}</p>
+          <span>By: {comment.author}</span> <span>votes: {comment.votes}</span>{" "}
+          <span>Created: {formatDate(comment.created_at)}</span>
+        </li>
+      );
+    });
+    setComments(commentValues);
+    setLoading(false);
+  }
 
-        const commentValues = comments.map((comment, index)=>{
-            return <li className={'comment-card'} key={index + comment.body}>
-                <p>{comment.body}</p>
-                <span>By: {comment.author}</span>  <span>votes: {comment.votes}</span> <span>Created: {formatDate(comment.created_at)}</span>
-            </li>
-        })
-        setComments(commentValues)
-        setLoading(false)
-    }
+  useEffect(() => {
+    getComments(id);
+  }, []);
 
-    useEffect(() => {
-        getComments(id)
-    }, []);
-
-
-    if(isLoading){
-        return <Loader/>
-    }
-    return <section>
-        <h3>Comments</h3>
-        <ol>
-            {comments}
-        </ol>
+  if (isLoading) {
+    return <Loader />;
+  }
+  return (
+    <section>
+      <h3>Comments</h3>
+      <ol className={"comments-holder"}>{comments}</ol>
     </section>
+  );
 }
 
-export default Comments
+export default Comments;
