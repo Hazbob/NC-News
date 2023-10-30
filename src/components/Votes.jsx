e47ca884e65d9e926acbadf3ed3f09baf16e1b6d
harryRobinson <127695266+Hazbob@users.noreply.github.com>
2023-10-30 22:19:15 +0000

    styling chnages to voting system

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,21 +1,48 @@
 import { useState } from "react";
-import {handleVotesChange} from "../api.jsx";
+import { handleVotesChange } from "../api.jsx";
+import {
+  BsFillArrowDownCircleFill,
+  BsFillArrowUpCircleFill,
+} from "react-icons/bs";
 
 function Votes({ votes, articleId }) {
-    const [currentVote, setCurrentVote] = useState(votes);
-    const [disabled, setDisabled] = useState(false);
+  const [currentVote, setCurrentVote] = useState(votes);
+  const [disabled, setDisabled] = useState(false);
+  const [btnColour, setBtnColour] = useState("red");
 
-    const changeVotes = (e) => {
-        handleVotesChange(currentVote, setCurrentVote, setDisabled, articleId, e.target.innerText);
-    };
-
-    return (
-        <span>
-            <button onClick={changeVotes} disabled={disabled}>-</button>
-            Votes: {currentVote}
-            <button onClick={changeVotes} disabled={disabled}>+</button>
-        </span>
+  const changeVotes = (e) => {
+    setBtnColour("grey");
+    handleVotesChange(
+      currentVote,
+      setCurrentVote,
+      setDisabled,
+      articleId,
+      e.target.innerText,
     );
+  };
+
+  return (
+    <span className={"votes-container"}>
+      <button
+        className={"vote-button"}
+        onClick={changeVotes}
+        disabled={disabled}
+      >
+        <BsFillArrowDownCircleFill
+          style={{ color: `${btnColour}` }}
+          size={50}
+        />
+      </button>
+      <h1>Votes: {currentVote}</h1>
+      <button
+        className={"vote-button"}
+        onClick={changeVotes}
+        disabled={disabled}
+      >
+        <BsFillArrowUpCircleFill size={50} style={{ color: `${btnColour}` }} />
+      </button>
+    </span>
+  );
 }
 
 export default Votes;
